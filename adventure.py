93f3ec4c474a222de363efcfb6831a13e892a437
EirikJN <eirikjoseph@gmail.com>
2023-09-06 00:35:19 -0600

    adventurepy

---
+++ adventure.py
@@ -0,0 +1,18 @@
+print("Det er en brann i huset, som vokser fort, men du er låst inne,")
+print("hva skal du gjøre? A: Finne nøkkelen, eller B: Bryte opp døra")
+
+valg = input("A eller B -> ")
+
+if valg == "A":
+    print("Nøkkelen")
+
+if valg == "B":
+    print("correct")
+    print("A B")
+    valg = input("A eller B -> ")
+
+    if valg == "A":
+        print("correct")
+
+    if valg == "B":
+        print("incorrect")
